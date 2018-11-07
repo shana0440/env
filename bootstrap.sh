098aed33d95c4b9dd6d5c424ecfa7285a82b3b10
kw <shana0440@gmail.com>
2018-11-07 19:10:04 +0800

    setup neovim & some alias

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source setup.sh
-
 # xcode
 xcode-select --install
 xcode-select -p
 
-source app.sh
+bash app.sh
 
 # 安裝並修改字型
 brew tap caskroom/fonts
@@ -16,6 +14,16 @@ brew cask install font-hack-nerd-font
 # setup git information
 git config --global user.name "kwguo"
 git config --global user.email shana0440@gmail.com
+
+# setup neovim
+mkdir ~/.config/nvim
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
+echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
+
+# alias
+echo "alias vim='nvim'" >> ~/.bashrc
+echo "alias glas='git log --all --graph --oneline --decorate'" >> ~/.bashrc
 
 # setup vim
 bash <(curl https://bit.ly/2Ojwlzi -L)
