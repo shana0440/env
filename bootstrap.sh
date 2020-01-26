2c8de35968a8537bdd34a1f72cd47b23226f61d5
kwguo <shana0440@gmail.com>
2020-01-26 21:13:01 +0800

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
+# brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # xcode
 xcode-select --install
 xcode-select -p
 
+# setup font
+brew tap caskroom/fonts
+brew cask install font-jetbrains-mono
+
+# install apps
 bash app.sh
 
-# 安裝並修改字型
-brew tap caskroom/fonts
-brew cask install font-hack-nerd-font
-# Profiles > Text > Change Font
-
-# setup git information
-git config --global user.name "kwguo"
-git config --global user.email shana0440@gmail.com
-
-# setup neovim
-mkdir ~/.config/nvim
-pip3 install neovim
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
-echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
-echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
-
-# alias
-echo "alias vim='nvim'" >> ~/.bashrc
-echo "alias glas='git log --all --graph --oneline --decorate'" >> ~/.bashrc
-
-# setup vim
-bash <(curl https://bit.ly/2Ojwlzi -L)
+git clone git@github.com:shana0440/dotfiles.git
+stow alacritty fish git tmux vim
