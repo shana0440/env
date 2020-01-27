07de676e07bf25fc53bbf7918555dddfc09e0578
kw <shana0440@gmail.com>
2020-01-27 14:22:27 +0800

    use https instead of git

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,5 +11,5 @@ brew cask install font-jetbrains-mono
 # install apps
 bash app.sh
 
-git clone git@github.com:shana0440/dotfiles.git
-stow alacritty fish git tmux vim yabai skhd
+git clone https://github.com/shana0440/dotfiles.git ~/dotfiles
+cd ~/dotfiles && stow alacritty fish git tmux vim yabai skhd && cd ~
